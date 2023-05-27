d9c0983ea7065460a0d225d9bca0ad7033631d3a
Richard Rietdijk <richardrietdijk@Richards-MacBook-Pro.local>
2023-05-27 08:24:56 -0500

    implement smart contract, FakeNFTMarketplace deployed to:  0x0fcDb6B3D5113966717723D1eAB4415D58ecF81D, CryptoDevsDAO deployed to:  0xc6eCACcE049b3791f0d45f8704b1b7C490882c46

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xF1fd385FBe7AC88eC0B13AF2c7b86707Dc25EcA5";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -29,4 +29,11 @@ module.exports = {
 */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_HTTP_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
